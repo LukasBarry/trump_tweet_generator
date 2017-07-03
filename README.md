c15d30844beb191be1199f476656f4df0c713270
LukasBarry <Lukasbarry@yahoo.com>
2017-07-03 10:01:07 -0400

    adds readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trump Tweet Generator
+
+This program takes the 'Presidents' most commonly used words, phrases, political figures, and organizations, along with his superfluous flourishing words to generate a life-like Trump tweet.
+
+Now you can be just as eloquent as the 45th President of the United States!
--- trump_tweets.rb
+++ trump_tweets.rb
@@ -3,17 +3,21 @@ SHAME_WORD = [
   'Mean'
 ].freeze
 POLITICAL_FIGURES = ['Obama', 'Comey'].freeze
-ORGANIZATIONS = ['Media', 'CNN', 'Fox', 'NBC'].freeze
-ADVERBS = ['very', 'much', 'more'].freeze
-ADJECTIVE = ['prestigious', 'stupid', 'weak'].freeze
-NOUN = ['loser', 'hater'].freeze
+ORGANIZATIONS = ['Media', 'CNN', 'Fox', 'NBC', 'Breitbart'].freeze
+ADVERBS = ['very', 'much', 'more', 'really'].freeze
+ADJECTIVE = [
+  'prestigious', 'stupid', 'weak', 'boring', 'bad', 'amazing', 'controlling',
+  'huge'
+].freeze
+NOUN = ['loser', 'hater', 'moron', 'lightweight'].freeze
 PARTIAL_SENTENCES = [
   'I have the best', 'I supported', 'I am being dscriminated against by',
   'I never liked'
 ].freeze
 PARTIAL_SENTENCES2 = [
   'is dscriminating against me', 'is a liar', 'is so mean', 'is unfair',
-  'is wrong', 'wants to take your jobs away', 'wants to inact Sharia Law'
+  'is wrong', 'wants to take your jobs away', 'wants to inact Sharia Law',
+  'supports crime'
 ].freeze
 
 # Trump tweet formula:
@@ -23,4 +27,4 @@ PARTIAL_SENTENCES2 = [
 
 puts "#{PARTIAL_SENTENCES.sample} #{ORGANIZATIONS.sample}. #{NOUN.sample.capitalize}. " \
      "#{POLITICAL_FIGURES.sample} #{PARTIAL_SENTENCES2.sample}. #{ADVERBS.sample.capitalize} " \
-     "#{ADJECTIVE.sample}. #{SHAME_WORD.sample}"
+     "#{ADJECTIVE.sample}. #{SHAME_WORD.sample}!"
